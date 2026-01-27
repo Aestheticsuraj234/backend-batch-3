3989023ce1f94cda5ce1049e6cf6f3b638549f0c
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-01-27 20:47:25 +0530

    added

---
+++ authentication/Oauth/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRouter from './auth.js';
+
+dotenv.config();
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send(`<a href="/auth/google">Login with Google</a>`);
+});
+
+app.use("/auth" , authRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
